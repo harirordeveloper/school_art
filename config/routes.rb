7fd90d089412720d95a0e42a461261f5a7543bba
hari <harirordeveloper@gmail.com>
2020-05-08 21:43:57 +0530

    removed subdomain to test

--- config/routes.rb
+++ config/routes.rb
@@ -8,7 +8,7 @@ Rails.application.routes.draw do
   Doorkeeper::OAuth::TokenResponse.send :prepend, Doorkeeper::CustomTokenResponse
   Doorkeeper::OAuth::ErrorResponse.send :prepend, Doorkeeper::CustomTokenErrorResponse
   # devise_for :users
-  namespace :api, defaults: { format: :json }, constraints: { subdomain: 'api' }, path: '/'  do
+  namespace :api, defaults: { format: :json }, path: '/api'  do
     scope module: :v1, constraints: ApiConstraints.new(version: 1, default: true), path: '/v1' do
     	devise_for :users, controllers: {
            registrations: 'api/v1/users/registrations',
